b983ba71db46eba457acaa7076ac6dc0c50b7e97
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-01-13 00:23:49 +0100

    react posts list

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Posts from './Components/Posts';
+
+function App() {
+  return (
+    <div className="App">
+    
+    {/* <h1>Home page</h1> */}
+
+    <Posts/>
+    </div>
+  );
+}
+
+export default App;
