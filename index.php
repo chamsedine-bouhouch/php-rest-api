30eb8e07056647a2c44e8fde9145b1fc30ae77ec
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-01-12 20:05:41 +0100

    api.php routes

--- index.php
+++ index.php
@@ -1,33 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+require 'routes/api.php';
 
-use Bouhouch\PhpRestApi\Controller\Home;
 use Bouhouch\PhpRestApi\Lib\App;
-use Bouhouch\PhpRestApi\Lib\{Router, Response, Request};
-use Bouhouch\PhpRestApi\Model\Post;
-
-Post::load();
-
-Router::get('/', function () {
-    (new Home())->indexAction();
-});
-
-Router::get('/post', function (Request $req, Response $res) {
-    $res->toJSON(Post::all());
-});
-
-Router::post('/post', function (Request $req, Response $res) {
-    $post = Post::add($req->getJSON());
-    $res->status(201)->toJSON($post);
-});
-
-Router::get('/post/([0-9]*)', function (Request $req, Response $res) {
-    $post = Post::findById($req->params[0]);
-    if ($post) {
-        $res->toJSON($post);
-    } else {
-        $res->status(404)->toJSON(['error' => "Not Found"]);
-    }
-});
 
 App::run();
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Bouhouch\PhpRestApi\Controller\Home;
+use Bouhouch\PhpRestApi\Model\Post;
+use Bouhouch\PhpRestApi\Lib\{Router, Response, Request};
+
+Post::load();
+
+Router::get('/', function () {
+    (new Home())->indexAction();
+});
+
+Router::get('/post', function (Request $req, Response $res) {
+    $res->toJSON(Post::all());
+});
+
+Router::post('/post', function (Request $req, Response $res) {
+    $post = Post::add($req->getJSON());
+    $res->status(201)->toJSON($post);
+});
+
+Router::get('/post/([0-9]*)', function (Request $req, Response $res) {
+    $post = Post::findById($req->params[0]);
+    if ($post) {
+        $res->toJSON($post);
+    } else {
+        $res->status(404)->toJSON(['error' => "Not Found"]);
+    }
+});
