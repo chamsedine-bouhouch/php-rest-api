5e9e522b7adc36f0c1290c8e52a6efdf1fbcbf06
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-01-12 19:34:54 +0100

    Post Crud

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -5,8 +5,8 @@ namespace Bouhouch\PhpRestApi\Controller;
 class Home
 {
     public function indexAction()
-    { 
-        // you could add the twig package 'composer require "twig/twig:^2.0"' 
+    {
+        // you could add the twig package 'composer require "twig/twig:^2.0"'
         // and use it as "echo $twig->render('index', ['name' => 'Fabien']);"
         echo 'Hello from Controller';
     }
---
+++ src/Model/Post.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Bouhouch\PhpRestApi\Model;
+
+use Bouhouch\PhpRestApi\Lib\Config;
+
+class Post
+{
+    private static $DATA = [];
+
+    public static function all()
+    {
+        return self::$DATA;
+    }
+
+    public static function add($post)
+    {
+        // var_dump($post);
+        // var_dump($_POST);
+        $post->id = count(self::$DATA) + 1;
+        self::$DATA[] = $post;
+        self::save();
+        return $post;
+    }
+
+    public static function findById(int $id)
+    {
+        foreach (self::$DATA as $post) {
+            if ($post->id === $id) {
+                return $post;
+            }
+        }
+        return [];
+    }
+
+    public static function load()
+    {
+        $DB_PATH = Config::get('DB_PATH', __DIR__ . '/../../db.json');
+        self::$DATA = json_decode(file_get_contents($DB_PATH));
+    }
+
+    public static function save()
+    {
+        $DB_PATH = Config::get('DB_PATH', __DIR__ . '/../../db.json');
+        file_put_contents($DB_PATH, json_encode(self::$DATA, JSON_PRETTY_PRINT));
+    }
+}
